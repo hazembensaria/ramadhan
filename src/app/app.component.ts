63d3938478a185e88d90a32128934a48657a6ede
hazembensaria <bensariahazem@gmail.com>
2024-03-19 17:51:24 +0100

    add tailwind and nav-bar component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
+import { NavBarComponent } from './shared/nav-bar/nav-bar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet , ButtonModule],
+  imports: [RouterOutlet , ButtonModule ,NavBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
